9be64a5d28c1e932b15c72b862951ec45d58e2f4
Mauro A Flores A <mauroa_99@hotmail.com>
2016-05-12 21:27:56 -0500

    Initial Commit

---
+++ Prueba gitHub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prueba gitHub
+//
+//  Created by Mauro Alberto Flores Almonte on 12/05/16.
+//  Copyright © 2016 Mauro Alberto Flores Almonte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
